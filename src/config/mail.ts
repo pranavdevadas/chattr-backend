d047898a548324b6fb2d88f8cde52b3aa3206c39
pranavvv777 <pranavdevadas2@gmail.com>
2025-10-30 18:07:45 +0530

    modified nodemailer

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,4 +1,10 @@
 import nodemailer from "nodemailer";
+import sgMail from '@sendgrid/mail'
+import dotenv from "dotenv";
+
+dotenv.config();
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
 export const transporter = nodemailer.createTransport({
   // host: process.env.EMAIL_HOST,
@@ -18,17 +24,34 @@ export const transporter = nodemailer.createTransport({
   },
 });
 
+// export const sendMail = async (to: string, subject: string, text: string) => {
+//   try {   
+//     let mail = await transporter.sendMail({
+//       from: `"Chattr" <pranavdevadas2@gmail.com>`,
+//       to,
+//       subject,
+//       text,
+//     });
+//     console.log('mail', mail);
+//   } catch (err: any) {
+//     console.error("Error sending email:", err.message);
+//     throw new Error("Failed to send email");
+//   }
+// };
+
 export const sendMail = async (to: string, subject: string, text: string) => {
-  try {   
-    let mail = await transporter.sendMail({
-      from: `"Chattr" <pranavdevadas2@gmail.com>`,
+  try {
+    const msg = {
       to,
+      from: 'no-reply@chattr.com', // This must match a verified sender domain in SendGrid
       subject,
       text,
-    });
-    console.log('mail', mail);
+    };
+
+    const response = await sgMail.send(msg);
+    console.log('Email sent successfully:', response[0].statusCode);
   } catch (err: any) {
-    console.error("Error sending email:", err.message);
-    throw new Error("Failed to send email");
+    console.error('Error sending email:', err.response?.body || err.message);
+    throw new Error('Failed to send email');
   }
 };
