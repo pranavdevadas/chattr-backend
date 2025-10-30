c8fac814298207ed844308d47b3ad644c4df09cf
pranavvv777 <pranavdevadas2@gmail.com>
2025-10-30 16:42:39 +0530

    modified nodemailer

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -3,20 +3,24 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   // host: process.env.EMAIL_HOST,
   // port: Number(process.env.EMAIL_PORT),
-  service: "gmail",
+  // service: "gmail",
+  // secure: false,
+  // auth: {
+  //   user: "pranavdevadas2@gmail.com",
+  //   pass: "petd siof jlcc stcl",
+  // },
+  host: "smtp-relay.brevo.com",
+  port: 587,
   secure: false,
   auth: {
-    user: "pranavdevadas2@gmail.com",
-    pass: "petd siof jlcc stcl",
+    user: "9a6041001@smtp-brevo.com", 
+    pass: "Hr9tB2xYIqVWa3jd",
   },
-  pool: true,
-  connectionTimeout: 10000,
-  socketTimeout: 10000,
 });
 
 export const sendMail = async (to: string, subject: string, text: string) => {
   await transporter.sendMail({
-    from: `"Chattr" <pranavdevadas2@gmail.com>`,
+    from: `"Chattr" <no-reply@chattr.com>`,
     to,
     subject,
     text,
